4f0f7c89a7bc67fbc164ac9d85167ef025d8b480
Auriane Viggiano <auriane.vigg@gmail.com>
2024-02-10 16:06:06 +0100

    css footer and navbar

--- anemone_coffee/src/components/footer/Footer.jsx
+++ anemone_coffee/src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next'
-import {NavLink} from 'react'
-import './footer.css'
+import './footer.scss'
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 function Footer (props) {
@@ -9,22 +8,24 @@ function Footer (props) {
     return (
         <div className="footer_container">
             
-            <div className="footer_img">
-                <img className="footer_logo" src="/logos/LogoAnemone_blanc.png"/>
-                
-            </div>
-            <div className="footer_adress">
-                <h3>{t('footer.titleAdress')}</h3>
-                <p>{t('footer.adress')}</p>
-            </div>
-            <div className="footer_contact">
-                <h3>{t('footer.contact')}</h3>
-                <p>06 00 00 00 00</p>
-            </div>
-            <div className="footer_propos">
-                <h3>{t('footer.propos')}</h3>
-                <p>lien 1</p>
-                <p>lien 2</p>
+                <div className="footer_img">
+                    <img className="footer_logo" src="/logos/LogoAnemone_blanc.png"/>  
+                </div>
+
+                <div className="footer_container_content">
+                    <div className="footer_adress">
+                        <h3>{t('footer.titleAdress')}</h3>
+                        <p>{t('footer.adress')}</p>
+                    </div>
+                    <div className="footer_contact">
+                        <h3>{t('footer.contact')}</h3>
+                        <p>06 00 00 00 00</p>
+                    </div>
+                    <div className="footer_propos">
+                        <h3>{t('footer.propos')}</h3>
+                        <p>lien 1</p>
+                        <p>lien 2</p>
+                    </div>
             </div>
         </div>
     )
